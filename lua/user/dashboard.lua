6ef4dfac649c567c7729800147295490d5008b61
georgia-martinez <gcm49@case.edu>
2023-01-01 03:15:54 -0600

    Set up dashboard

---
+++ lua/user/dashboard.lua
@@ -0,0 +1,49 @@
+local db = require('dashboard')
+local home = os.getenv('HOME')
+
+db.default_banner = {
+  '',
+  '',
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+  '',
+  '',
+}
+
+db.preview_file_height = 11
+db.preview_file_width = 70
+
+db.custom_center = {
+    {icon = '  ',
+    desc = 'Recent sessions                         ',
+    shortcut = 'SPC s l',
+    action ='SessionLoad'},
+    {icon = '  ',
+    desc = 'Find recent files                       ',
+    action = 'Telescope oldfiles',
+    shortcut = 'SPC f r'},
+    {icon = '  ',
+    desc = 'Find files                              ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'},
+    {icon = '  ',
+    desc ='File browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'},
+    {icon = '  ',
+    desc = 'Find word                               ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'},
+    {icon = '  ',
+    desc = 'Change color scheme                     ',
+    action = 'Telescope colorscheme',
+    shortcut = 'SPC h t'},
+  }
+
+db.session_directory = "/home/dt/.config/nvim/session"
+
+
